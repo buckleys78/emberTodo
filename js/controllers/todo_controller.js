f5710db9ed011ea20a3abd74a764a0998fba2807
Steve Buckley <buckleys78@gmail.com>
2014-06-14 09:19:34 -0700

    show_edit toggle works

--- js/controllers/todo_controller.js
+++ js/controllers/todo_controller.js
@@ -1,4 +1,12 @@
 Todos.TodoController = Ember.ObjectController.extend({
+  actions: {
+    editTodo: function() {
+      this.set('isEditing', true);
+    }
+  },
+
+  isEditing: false,
+
   isCompleted: function(key, value){
     var model = this.get('model');
 
@@ -11,6 +19,5 @@ Todos.TodoController = Ember.ObjectController.extend({
       model.save();
       return value;
     }
-
   }.property('model.isCompleted')
 });

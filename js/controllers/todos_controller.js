8f74b262bfafcef76d657758a5c58f071e44ee67
Steve Buckley <buckleys78@gmail.com>
2014-06-14 10:59:43 -0700

    todos complete

--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -9,15 +9,36 @@ Todos.TodosController = Ember.ArrayController.extend({
         title: title,
         isCompleted: false
       });
-
       this.set('newTitle', '');
-
       todo.save();
+    },
+
+    clearCompleted: function() {
+      var completed = this.filterBy('isCompleted', true);
+      completed.invoke('deleteRecord');
+      completed.invoke('save');
     }
   },
+  hasCompleted: function() {
+    return this.get('completed') > 0;
+  }.property('completed'),
+
+  completed: function() {
+    return this.filterBy('isCompleted', true).get('length');
+  }.property('@each.isCompleted'),
 
   remaining: function() {
     return this.filterBy('isCompleted', false).get('length');
+  }.property('@each.isCompleted'),
+
+  allAreDone: function(key, value) {
+    if (value === undefined) {
+      return !!this.get('length') && this.isEvery('isCompleted');
+    }else{
+      this.setEach('isCompleted', value);
+      this.invoke('save');
+      return value;
+    }
   }.property('@each.isCompleted'),
 
   inflection: function() {
